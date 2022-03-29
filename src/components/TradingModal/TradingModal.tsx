e6e5c074f61de67627be9d952841481d73d10e8f
stemerlini <s.merlini19@imperial.ac.uk>
2022-03-30 00:02:40 +0100

    started the make offer modal design --> require fixing the inputbox and styling

--- src/components/TradingModal/TradingModal.tsx
+++ src/components/TradingModal/TradingModal.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Trading-Modal.css'
 import { OpenseaAsset } from '../../types/OpenseaAsset';
-
 
 export interface TradingModalProps {
     asset: OpenseaAsset;
@@ -12,6 +11,8 @@ export const TradingModal: React.FC<TradingModalProps> = ({
     asset,
     index
 }) => {
+    const [price, setPrice] = useState('')
+
     return (
         <div
         id={`lightbox-${index}`}
@@ -21,16 +22,22 @@ export const TradingModal: React.FC<TradingModalProps> = ({
         }}
       >
         <figure
-          className="perfundo__content perfundo__figure"
+          className="perfundo__figure"
           onClick={(evt) => {
             // Prevents clicks on the image triggering `#lightbox-untarget`.
             evt.stopPropagation();
           }}
         >
-            <div className='trading-container'>
-                <img className="perfundo__image" src={asset.image_url} loading="lazy" />
-                <div className=''> price and send </div>
-            </div>
+              <div className="perfundo__figtitle"> Make Offer </div>
+              <img className="perfundo__image" src={asset.image_url} loading="lazy" />
+              <div className='perfundo__figcaption'> 
+                <input  className='perfundo__inputbox' 
+                        placeholder='Amount...'
+                        value={price}
+                        onChange={()=>{setPrice()}}
+                />
+                <button onClick={() => alert('send request')} className='perfundo__inputbox'> Send </button>
+              </div>              
         </figure>
       </div>
     )
